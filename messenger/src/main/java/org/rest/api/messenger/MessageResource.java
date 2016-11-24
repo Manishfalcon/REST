651a4694f66c040d0ceb050f58d95f8bcaf8aaaf
Manish Sharma <manish.X.sharma.-ND@disney.com>
2016-11-24 19:11:12 +0530

    QueryParam implementation

--- messenger/src/main/java/org/rest/api/messenger/MessageResource.java
+++ messenger/src/main/java/org/rest/api/messenger/MessageResource.java
@@ -5,10 +5,15 @@ package org.rest.api.messenger;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.rest.api.messenger.model.Message;
@@ -19,21 +24,47 @@ import org.rest.api.messenger.service.MessageService;
  *
  */
 @Path ("messages")
+@Consumes (MediaType.APPLICATION_JSON)
+@Produces (MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	private MessageService service = new MessageService();
 	
 	@GET
-	@Produces (MediaType.APPLICATION_JSON)
-	public List<Message> getMessages(){
+	public List<Message> getMessages(@QueryParam("year") int year,
+									 @QueryParam("start") int start,
+									 @QueryParam("size") int size){
+		if(year > 0){
+			return service.getAllMessagesForYear(year);
+		}
+		if(start >=0 && size >0){
+			return service.getAllMessagesPaginated(start, size);
+		}
 		return service.getAllMessages();
 	}
 	
 	@GET
 	@Path("{messageId}")
-	@Produces (MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") final long messageId){
 		return service.getMessage(messageId);
 	}
 
+	@POST
+	public Message addMessage(final Message message){
+		return service.addMessage(message); 
+	}
+	
+	@PUT
+	@Path("{messageId}")
+	public Message updateMessage(@PathParam("messageId") final long messageId, final Message message){
+		message.setId(messageId);
+		return service.updateMessage(message); 
+	}
+	
+	@DELETE
+	@Path("{messageId}")
+	public void deleteMessage(@PathParam("messageId") final long id){
+		service.deleteMessage(id);
+	}
+	
 }
--- messenger/src/main/java/org/rest/api/messenger/service/MessageService.java
+++ messenger/src/main/java/org/rest/api/messenger/service/MessageService.java
@@ -4,6 +4,7 @@
 package org.rest.api.messenger.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +29,26 @@ public class MessageService {
 
 	public List<Message> getAllMessages(){
 		return new ArrayList<>(messages.values());
+	}
+	
+	public List<Message> getAllMessagesForYear(int year){
+		List<Message> messagesForYear = new ArrayList<>();
+		Calendar cal = Calendar.getInstance();
+		for (Message message : messages.values()){
+			cal.setTime(message.getCreated());
+			if(cal.get(Calendar.YEAR) == year){
+				messagesForYear.add(message);
+			}
+		}
+		return messagesForYear;
+	}
+	
+	public List<Message> getAllMessagesPaginated(int start, int size){
+		ArrayList<Message> list = new ArrayList<>(messages.values());
+		if(start + size > list.size()){
+			return new ArrayList<Message>();
+		}
+		return list.subList(start, start+size);
 	}
 	
 	public Message getMessage(long id){
